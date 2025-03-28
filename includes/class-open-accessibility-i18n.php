2adc4b5b5dc2ba93f81da17d2de8f589e78cdb40
Adam Greenwell <adamgreenwell@gmail.com>
2025-03-28 12:12:41 -0400

    Add direct file access protection

--- includes/class-open-accessibility-i18n.php
+++ includes/class-open-accessibility-i18n.php
@@ -1,4 +1,7 @@
 <?php
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
 /**
  * Define the internationalization functionality.
  *
@@ -13,6 +16,10 @@ class Open_Accessibility_i18n {
 
 	/**
 	 * Load the plugin text domain for translation.
+	 *
+	 * Note: Since WordPress 4.6+, translations are automatically loaded for plugins
+	 * hosted on WordPress.org. This method is kept for backward compatibility with
+	 * older WordPress versions.
 	 *
 	 * @since    1.0.0
 	 */
